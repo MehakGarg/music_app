dc59daae862cb7e97cb9d60f54974011b041d700
Mehak garg <mehak3996@gmail.com>
2017-07-21 10:17:39 +0530

    Final Commit

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
+
+
+
 var songs = [{
 										'name': 'Ik Vaari Aa',
 										'artist': 'Arijit Singh & Pritam',
@@ -62,6 +65,58 @@ var songs = [{
 										'fileName': 'song8.mp3',
 										'image' : 'song8.jpg'
 									}]
+
+
+
+
+
+
+var currentSongNumber = 1;
+var willLoop = 0;
+var willShuffle = 0;
+$('.fa-repeat').on('click',function() {
+    $('.fa-repeat').toggleClass('disabled')
+    willLoop = 1 - willLoop;
+});
+$('.fa-random').on('click',function() {
+    $('.fa-random').toggleClass('disabled')
+    willShuffle = 1 - willShuffle;
+});
+
+function timeJump() {
+    var song = document.querySelector('audio')
+    song.currentTime = song.duration - 5;
+}
+$('audio').on('ended',function() {
+    var audio = document.querySelector('audio');
+    if (willShuffle == 1) {
+        var nextSongNumber = randomExcluded(1,8,currentSongNumber); // Calling our function from Stackoverflow
+        var nextSongObj = songs[nextSongNumber-1];
+        audio.src = nextSongObj.fileName;
+        toggleSong();
+        changeCurrentSongDetails(nextSongObj);
+        currentSongNumber = nextSongNumber;
+    }
+
+    else if(willLoop == 1) {
+        var nextSongObj = songs[0];
+        audio.src = nextSongObj.fileName;
+        toggleSong();
+        changeCurrentSongDetails(nextSongObj);
+        currentSongNumber =  1;
+    }
+    else {
+        $('.play-icon').removeClass('fa-pause').addClass('fa-play');
+        audio.currentTime = 0;
+    }
+})
+function randomExcluded(min, max, excluded) {
+    var n = Math.floor(Math.random() * (max-min) + min);
+    if (n >= excluded) n++;
+    return n;
+}
+
+
 
 									function fancyTimeFormat(time)
 								 					{
@@ -135,6 +190,10 @@ var songs = [{
 								                         }
 
 
+
+
+
+
 								 				                window.onload = function() {
 
 								 								changeCurrentSongDetails(songs[0]);
@@ -142,6 +201,7 @@ var songs = [{
 								 					         	updateCurrentTime();
 								 								setInterval(function() {
 								 								updateCurrentTime();
+
 								 								},1000);
 
 
@@ -173,13 +233,18 @@ var songs = [{
 								 									$('#songs').DataTable({
 								 									paging: false
 								 								});
+					 		                          }
+																				$('.fa-music').on('click',function(){
+																					$('.content').toggleClass('hidden');
 
-								 		                          }
+																					$('.Drumkit').toggleClass('hidden');
+																					console.log('hello');
 
+																				});
 
 								     $('.welcome-screen button').on('click', function() {
 								         var name = $('#name-input').val();
-								         if (name.length > 2) {
+								         if (name.length > 3) {
 								             var message = "Welcome, " + name;
 								             $('.main .user-name').text(message);
 								             $('.welcome-screen').addClass('hidden');
@@ -188,9 +253,13 @@ var songs = [{
 								             $('#name-input').addClass('error');
 								         }
 								     });
+
+
+
 								 						$('.play-icon').on('click', function() {
 								 							toggleSong();
 								 						});
+
 
 
 								 						$('body').on('keypress', function(event) {
@@ -198,3 +267,161 @@ var songs = [{
 								 										toggleSong();
 								 									}
 								 								});
+/*Drumkit*/
+
+
+	// Onclick functions
+	function clap() {
+		var clap = document.querySelector("audio");
+console.log(clap);
+		clap.src="sounds/clap.wav";
+console.log(clap.src);
+		clap.currentTime = 0;
+		clap.play();
+		const key = document.querySelector(".key[data-key='65']");
+		key.classList.add('playing'); // adds the css animation while its playing
+	}
+
+	function hihat() {
+
+		var hihat = document.querySelector("audio");
+		hihat.src="sounds/hihat.wav";
+		hihat.currentTime = 0;
+		hihat.play();
+		const key = document.querySelector(".key[data-key='83']");
+		key.classList.add('playing');
+	}
+	function kick() {
+		const kick = document.querySelector("audio");
+		kick.src="sounds/kick.wav"
+		kick.currentTime = 0;
+		kick.play();
+		const key = document.querySelector(".key[data-key='68']");
+		key.classList.add('playing');
+	}
+	function openhat() {
+		const openhat = document.querySelector("audio");
+		openhat.src="sounds/openhat.wav"
+		openhat.currentTime = 0;
+		openhat.play();
+		const key = document.querySelector(".key[data-key='70']");
+		key.classList.add('playing');
+	}
+	function boom() {
+		const boom = document.querySelector("audio");
+		boom.src="sounds/boom.wav"
+		boom.currentTime = 0;
+		boom.play();
+		const key = document.querySelector(".key[data-key='71']");
+		key.classList.add('playing');
+	}
+	function ride() {
+		const ride = document.querySelector("audio");
+		ride.src="sounds/ride.wav";
+		ride.currentTime = 0;
+		ride.play();
+		const key = document.querySelector(".key[data-key='72']");
+		key.classList.add('playing');
+	}
+	function snare() {
+		const snare = document.querySelector("audio");
+		snare.src="sounds/snare.wav"	;
+		snare.currentTime = 0;
+		snare.play();
+		const key = document.querySelector(".key[data-key='74']");
+		key.classList.add('playing');
+	}
+	function tom() {
+		const tom = document.querySelector("audio");
+		tom.src="sounds/tom.wav"
+		tom.currentTime = 0;
+		tom.play();
+		const key = document.querySelector(".key[data-key='75']");
+		key.classList.add('playing');
+	}
+	function tink() {
+		const tink = document.querySelector("audio");
+		tink.src="sounds/tink.wav"
+		tink.currentTime = 0;
+		tink.play();
+		const key = document.querySelector(".key[data-key='76']");
+		key.classList.add('playing');
+
+	}
+
+function removeTransition(e) {
+  if(e.propertyName !== 'transform') return; // skip it if it's not a transform
+  this.classList.remove('playing');
+}
+
+const keys = document.querySelectorAll('.key');
+keys.forEach(key => key.addEventListener('transitionend',removeTransition));
+
+
+
+$("body").on("keypress",function(e){
+
+if(e.keyCode==65)
+{
+
+clap();
+
+}
+
+if(e.keyCode==83)
+{
+console.log("a");
+hihat();
+
+}
+
+if(e.keyCode==68)
+{
+console.log("a");
+kick();
+
+}
+
+if(e.keyCode==70)
+{
+console.log("a");
+openhat();
+
+}
+
+if(e.keyCode==71)
+{
+console.log("a");
+boom();
+
+}
+
+if(e.keyCode==72)
+{
+console.log("a");
+ride();
+
+}
+
+if(e.keyCode==74)
+{
+console.log("a");
+snare();
+
+}
+
+
+if(e.keyCode==75)
+{
+console.log("a");
+tom();
+
+}
+
+if(e.keyCode==76)
+{
+console.log("a");
+tink();
+
+}
+})
